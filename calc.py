652a629ec77499248d2e3c27753f249a4cbdb1eb
Test <test@test.local>
2026-09-15 21:19:31 -0400

    docs: document discount

--- calc.py
+++ calc.py
@@ -1,2 +1,3 @@
 def discount(total_cents: int, percent_off: int) -> int:
+    """Take percent_off percent off total_cents, flooring the discount."""
     return total_cents - (total_cents * percent_off) // 100
